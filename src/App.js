76cde869de941e35e8209be4f7d020796bc485c5
Samuel Vienne <samuelvienne@yahoo.fr>
2021-06-07 22:28:18 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+//Librairies
+import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+
+//Components
+import Layout from './hoc/Layout/Layout';
+import Home from './Containers/Home/Home';
+import Contact from './Components/Contact/Contact';
+import Articles from './Containers/Articles/Articles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/contact" component={Contact}/>
+          <Route exact path="/articles" component={Articles}/>
+        </Switch>
+      </Layout>
     </div>
   );
 }
